0c660c59e88790423769c66afde03af44aed8e7c
Leonardo Janardi <leojanardi@gmail.com>
2019-12-06 01:17:34 -0300

    Commit de implementação de novas funcionalidades e inicio de desenvolvimento de regras de autenticação e permissão de processamento

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,48 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-unused-vars */
+import * as Yup from 'yup';
+import jwt from 'jsonwebtoken';
+import User from '../models/User';
+import AuthConfig from '../../config/auth';
+
+class SessionController {
+  async login(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ error: 'User not found' });
+    }
+
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name } = user;
+
+    return res.json({
+      user: {
+        id,
+        name,
+        email,
+      },
+      token: jwt.sign({ id }, AuthConfig.secret, {
+        expiresIn: AuthConfig.expiresIn,
+      }),
+    });
+  }
+}
+
+export default new SessionController();
---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,29 @@
+/* eslint-disable require-atomic-updates */
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+import authConfig from '../../config/auth';
+
+export default async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token not provided' });
+  }
+
+  if (req.authUser !== 'admin@gympoint.com') {
+    return res
+      .status(401)
+      .json({ error: 'This user does not have enought privileges' });
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+    req.userId = decoded.id;
+
+    return next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Invalid Token' });
+  }
+};
